a87cfbbdb1ed368578244a7eaba389504bdb3f2e
weneverchange <chromaglowenquires@gmail.com>
2024-01-09 07:42:19 -0300

    fix formatting

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+const { token } = require('./config.json')
+const fs = require('node:fs')
+const path = require('node:path')
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-client.commands = new Collection();
+client.commands = new Collection()
 
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const foldersPath = path.join(__dirname, 'commands')
+const commandFolders = fs.readdirSync(foldersPath)
 
 for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
+  const commandsPath = path.join(foldersPath, folder)
+  const commandFiles = fs.readdirSync(commandsPath).filter((file) =>
+    file.endsWith('.js'))
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file)
+    const command = require(filePath)
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command)
+    } else {
+      console.log(`[WARNING] The command at ${filePath} ` +
+                  'is missing a required "data" or "execute" property.')
+    }
+  }
 }
 
 client.on(Events.InteractionCreate, async (interaction) => {
-	if (!interaction.isChatInputCommand()) return;
+  if (!interaction.isChatInputCommand()) return
 
-	const command = interaction.client.commands.get(interaction.commandName);
+  const command = interaction.client.commands.get(interaction.commandName)
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-	}
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`)
+  }
 
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an erro while executing the command!', ephemeral: true });
-		}
-	}
-});
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    console.error(error)
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: 'There was an error while executing the command!',
+        ephemeral: true
+      })
+    }
+  }
+})
 
-client.once(Events.ClientReady, (ready) => console.log(`Ready ${ready.user.tag}`));
+client.once(Events.ClientReady, (ready) =>
+  console.log(`Ready ${ready.user.tag}`))
 
-client.login(token);
+client.login(token)
